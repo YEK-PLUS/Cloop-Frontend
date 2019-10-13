e0e6068947f7a1e8b2dca8e191875a83dd501df3
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-13 22:45:35 +0300

    eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "indent": [2, "tab"],
+    "no-tabs": 0
+  },
+};

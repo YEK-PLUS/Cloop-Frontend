61b49a2d5142e882b4b965dd068952e9535b9e53
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-16 22:57:07 +0300

    updated button component

--- src/components/buttons/roundedButton.js
+++ src/components/buttons/roundedButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 const RoundedButton = (props) => (
-  <div className={`animation w-full py-4 px-3 bg-${props.bg}-500 hover:bg-${props.bg}-400`}
+  <div className={`animation w-auto m-auto py-4 px-3 bg-${props.bg}-500 hover:bg-${props.bg}-400`}
     onClick={_.get(props,['onChange'])}>
     {_.get(props,['text'])}
   </div>

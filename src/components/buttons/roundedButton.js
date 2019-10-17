7e2c21d9d599f508c838697253280c73b47a3cb2
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-17 20:54:06 +0300

    renamed js to jsx

--- src/components/buttons/roundedButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import _ from 'lodash';
-const RoundedButton = (props) => (
-  <div className={`animation cursor-pointer text-white font-bold text-lg rounded w-auto m-auto py-2 px-5 bg-${props.bg}-500 hover:bg-${props.bg}-400`}
-    onClick={_.get(props,['onClick'])}>
-    {_.get(props,['text'])}
-  </div>
-)
-export default RoundedButton

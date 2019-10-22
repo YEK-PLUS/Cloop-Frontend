3180021105f27379d8fa7641fc926aa5031477dc
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-22 21:21:12 +0300

    updated apollo imp system

--- src/api/apollo.js
+++ src/api/apollo.js
@@ -1,8 +1,12 @@
 import ApolloClient from 'apollo-boost';
+import { getLocalStorage } from '../helper';
 import 'cross-fetch/polyfill';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:8080/graphql',
+	uri: 'http://192.168.1.105:8080/graphql',
+	headers: {
+		authorization: getLocalStorage('UserToken'),
+	},
 });
 
 export default client;
